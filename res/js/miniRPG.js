186df09ac6d80991b307e1c70077d830ad6a1340
tade18 <tadeaskeller@gmail.com>
2023-11-16 20:52:19 +0100

    update7

--- res/js/miniRPG.js
+++ res/js/miniRPG.js
@@ -3,12 +3,15 @@ const hp2 = document.getElementById("hp2");
 const attack = document.getElementById("attack");
 const audio = new Audio('/res/audio/audio.mp3');
 const info = document.getElementById("info");
-let health = document.getElementById("health");
+let health2 = document.getElementById("health2");
+let health1 = document.getElementById("health1");
 const dmg1 = document.getElementById("dmg1");
 const dmg2 = document.getElementById("dmg2");
 
 let hpPO = 1000;
 let hpPT = 1000;
+
+let pTwoAttack
 
 let dmgO = 14;
 let dmgT = 13;
@@ -19,12 +22,25 @@ attack.onclick = () => {
     if(hpPT >= 0){
     console.log("P1: útok");
     hpPT -= dmgO;
-    health.value -= dmgO;
+    health2.value -= dmgO;
     hp2.innerText="Health:" +hpPT;
     if (hpPT<=0){
     console.log("P1: vyhrává souboj")
-    info.innerText="Status: Kitty vyhrává...";
+    info.innerText="Status: Kitty vyhrává souboj!";
     info.style.color="green";
     }
     }
+}
+if(hpPO >= 0){
+    clearInterval(pTwoAttack);
+    pTwoAttack = setInterval(()=>{
+        hpPO -= dmgT;
+        health1.value -= dmgT;
+        hp1.innerText="Health:"+hpPO;
+        if(hpPO <= 0){
+            info.innerText="Status: Slovák vyhrává souboj!";
+            info.style.color="red";
+            clearInterval(pTwoAttack);
+        }
+        },1000)
 }
