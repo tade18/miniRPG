ad4180a02870e6aa86d815686ab001fb57edbf95
tade18 <tadeaskeller@gmail.com>
2023-11-16 22:04:28 +0100

    update9

--- res/js/miniRPG.js
+++ res/js/miniRPG.js
@@ -1,34 +1,39 @@
 const hp1 = document.getElementById("hp1");
 const hp2 = document.getElementById("hp2");
 const attack = document.getElementById("attack");
-const audio = new Audio('/res/audio/audio.mp3');
-const sword_sound = new Audio('/res/audio/sword_sound.mp3');
+const audio = new Audio('../audio/audio.mp3');
+const sword_sound = new Audio('../audio/sword_sound.mp3');
 const info = document.getElementById("info");
 let health2 = document.getElementById("health2");
 let health1 = document.getElementById("health1");
 const dmg1 = document.getElementById("dmg1");
 const dmg2 = document.getElementById("dmg2");
 const startButton = document.getElementById("StartButton");
+//schová element
 const hideElement = (element) => {
     element.style.display = "none";
 }
+//zobrazit element v původním stavu
 const showElement = (element) => {
     element.style.display = "initial"
 }
+//životy
 let hpPO = 1000;
 let hpPT = 1000;
-
+//attack protivníka
 let pTwoAttack
-
+//damage
 let dmgO = 6;
 let dmgT = 12;
 
-hideElement(attack)
 
+
+hideElement(attack)
+//zahájení hry
 startButton.onclick = () => {
     audio.play();
     hideElement(startButton);
-    showElement(attack)
+    showElement(attack);
     if(hpPO >= 0){
     clearInterval(pTwoAttack);
     pTwoAttack = setInterval(()=>{
